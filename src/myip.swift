2ba38ff59fef51fe7177de98e6da823ed7e82763
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2026-08-25 16:56:49 +0300

    Update myip.swift

--- src/myip.swift
+++ src/myip.swift
@@ -35,15 +35,28 @@ public class Myip {
         ]
 
     }
-    
-    public func get_ip_info(ip: String) async throws -> Any {
-        guard let url = URL(string: "\(api)/\(ip)") else {
+
+
+    private func fetchJSON(from urlString: String,method: HTTPMethod = .get,body: Data? = nil,queryParameters: [String: String]? = nil) async throws -> Any {
+        var urlComponents = URLComponents(string: urlString)
+        if let queryParameters = queryParameters {
+            urlComponents?.queryItems = queryParameters.map { URLQueryItem(name: $0.key, value: $0.value) }
+        }
+        guard let url = urlComponents?.url else {
             throw NSError(domain: "Invalid URL", code: -1)
         }
         var request = URLRequest(url: url)
-        request.httpMethod = "GET"
+        request.httpMethod = method.rawValue
         request.allHTTPHeaderFields = headers
+        if let body = body {
+            request.httpBody = body
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        }
         let (data, _) = try await URLSession.shared.data(for: request)
         return try JSONSerialization.jsonObject(with: data)
+    }
+    
+    public func getIpInfo(ip: String) async throws -> Any {
+        return try await fetchJSON(from: "\(api)/\(ip)")
     }
 }
